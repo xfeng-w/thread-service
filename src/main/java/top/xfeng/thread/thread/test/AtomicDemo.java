b25856db15bef02d0c9408ac78b65dd955e41754
xuefeng.wang <Ty123123>
2020-07-20 16:18:42 +0800

    add AtomicDemo

---
+++ src/main/java/top/xfeng/thread/thread/test/AtomicDemo.java
@@ -0,0 +1,32 @@
+package top.xfeng.thread.thread.test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author xuefeng.wang
+ * @date 2020-07-20
+ */
+public class AtomicDemo {
+
+    private AtomicInteger stock = new AtomicInteger(0);
+
+    public void setStock(AtomicInteger stock) {
+        this.stock = stock;
+    }
+
+    public AtomicInteger getStock() {
+        return stock;
+    }
+
+    public static void main(String[] args) {
+        AtomicDemo demo = new AtomicDemo();
+        for (int i = 0; i < 1000; i++) {
+            new Thread(demo::updateStock, "user" + i).start();
+        }
+    }
+
+    private void updateStock() {
+        stock.getAndAdd(1);
+        System.out.println("当前库存：" + stock);
+    }
+}
